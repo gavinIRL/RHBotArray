474218e73bbe98c28a00605d254cba0c948351c0
Gavin <gavin.feeney01@gmail.com>
2021-05-02 12:08:33 +0100

    Add init and start to client test

--- 02_client_test.py
+++ 02_client_test.py
@@ -6,4 +6,27 @@ import _thread
 
 class ClientTest():
     def __init__(self) -> None:
-        pass
+        with open("ip.txt") as f:
+            ip = f.readline()
+        port = 1351
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((ip, port))
+        self.sockets_list = []
+
+    def start(self):
+        while True:
+            self.sockets_list = [sys.stdin, self.server]
+            read_socks, write_sock, err_sock = select.select(
+                self.sockets_list, [], [])
+
+            for socks in read_socks:
+                if socks == self.server:
+                    message = socks.recv(2048)
+                    print(message)
+                else:
+                    message = sys.stdin.readline()
+                    self.server.send(message)
+                    sys.stdout.write("<You>")
+                    sys.stdout.write(message)
+                    sys.stdout.flush()
