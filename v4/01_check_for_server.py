dd6c4a90b3414f5e2d0ad99e4bc63485c03dd78f
Gavin <gavin.feeney01@gmail.com>
2021-05-02 20:43:03 +0100

    Update ip checks to be functions #5

--- v4/01_check_for_server.py
+++ v4/01_check_for_server.py
@@ -2,21 +2,29 @@
 from subprocess import check_output
 from subprocess import run
 import socket
-output = run("arp -a", capture_output=True).stdout.decode()
-# print(output)
-
-list_ips = []
-with open("servers.txt", "r") as f:
-    lines = f.readlines()
-    for ip in lines:
-        if ip.strip() in output:
-            list_ips.append(ip.strip())
-
-print(list_ips)
 
 
-output2 = run("ipconfig", capture_output=True).stdout.decode()
-_, post = output2.split("IPv4 Address. . . . . . . . . . . : 192")
-final, _ = post.split("Subnet Mask")
-current_lan_ip = "192" + final
-print(current_lan_ip)
+def grab_online_servers():
+    output = run("arp -a", capture_output=True).stdout.decode()
+    list_ips = []
+    with open("servers.txt", "r") as f:
+        lines = f.readlines()
+        for ip in lines:
+            if ip.strip() in output:
+                list_ips.append(ip.strip())
+    return list_ips
+
+
+print(grab_online_servers())
+
+
+def grab_current_lan_ip():
+    output = run(
+        "ipconfig", capture_output=True).stdout.decode()
+    _, output = output.split("IPv4 Address. . . . . . . . . . . : 192")
+    output, _ = output.split("Subnet Mask")
+    current_lan_ip = "192" + output.strip()
+    return current_lan_ip
+
+
+print(grab_current_lan_ip())
