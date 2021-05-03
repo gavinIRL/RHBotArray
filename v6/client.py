3a1feb9c1793a59285cd44b09f990637e5078118
Gavin <gavin.feeney01@gmail.com>
2021-05-03 19:39:07 +0100

    Add client handling of scaling for #6 #8

--- v6/client.py
+++ v6/client.py
@@ -3,6 +3,9 @@ import select
 import errno
 import sys
 from win32gui import GetWindowText, GetForegroundWindow
+from win32api import GetSystemMetrics
+from windowcapture import WindowCapture
+import ctypes
 from pynput.keyboard import Key, Listener, KeyCode
 from pynput import mouse, keyboard
 from random import randint
@@ -10,6 +13,8 @@ import subprocess
 import threading
 import time
 import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 class RHBotClientConnection():
@@ -65,8 +70,12 @@ class ClientKeypressListener():
         self.list_servers = list_servers
         self.listener = None
         self.unreleased_keys = []
+
+        self.scaling = ClientUtils.get_monitor_scaling()
         with open("gamename.txt") as f:
             self.gamename = f.readline()
+        if self.test:
+            self.game_wincap = WindowCapture(self.gamename)
 
     def start_mouse_listener(self):
         if not self.test:
@@ -86,9 +95,9 @@ class ClientKeypressListener():
             if not pressed:
                 # Need to get the ratio compared to window top left
                 # This will allow common usage on other size monitors
-                # xratio, yratio = self.convert_click_to_ratio(x, y)
+                xratio, yratio = self.convert_click_to_ratio(x, y)
                 for server in self.list_servers:
-                    server.send_message("click,"+str(x)+"|"+str(y))
+                    server.send_message("click,"+str(xratio)+"|"+str(yratio))
 
     def start_keypress_listener(self):
         if self.listener == None:
@@ -118,6 +127,36 @@ class ClientKeypressListener():
             for server in self.list_servers:
                 server.send_message(str(key)+",up")
             self.unreleased_keys.remove(str(key))
+
+    def convert_click_to_ratio(self, truex, truey):
+        # This will grab the current rectangle coords of game window
+        # and then turn the click values into a ratio of positions
+        # versus the game window
+        self.game_wincap.update_window_position(border=False)
+        # Turn the screen pos into window pos
+        relx = truex - self.game_wincap.window_rect[0] * self.scaling
+        rely = truey - self.game_wincap.window_rect[1] * self.scaling
+        # print("relx={}, rely={}".format(relx, rely))
+        # print("winx={}, winy={}".format(
+        #     self.game_wincap.window_rect[0], self.game_wincap.window_rect[1]))
+        # print("winwidth={}".format(self.game_wincap.w))
+        # Then convert to a ratio
+        ratx = relx/(self.game_wincap.w*self.scaling)
+        raty = rely/(self.game_wincap.h*self.scaling)
+        return ratx, raty
+
+    def convert_ratio_to_click(self, ratx, raty):
+        # This will grab the current rectangle coords of game window
+        # and then turn the ratio of positions versus the game window
+        # into true x,y coords
+        self.game_wincap.update_window_position(border=False)
+        # Turn the ratios into relative
+        relx = int(ratx * self.game_wincap.w)
+        rely = int(raty * self.game_wincap.h)
+        # Turn the relative into true
+        truex = int((relx + self.game_wincap.window_rect[0]) * self.scaling)
+        truey = int((rely + self.game_wincap.window_rect[1]) * self.scaling)
+        return truex, truey
 
     def on_click_test(self, x, y, button, pressed):
         # when pressed is False, that means it's a release event.
@@ -162,6 +201,13 @@ class ClientUtils():
         t = threading.Thread(target=server.main_loop(), daemon=True)
         t.start()
 
+    def get_monitor_scaling(self):
+        user32 = ctypes.windll.user32
+        w_orig = GetSystemMetrics(0)
+        user32.SetProcessDPIAware()
+        [w, h] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
+        return float(("{:.2f}".format(w/w_orig)))
+
 
 class RHBotClient():
     def start(delay_min=0, delay_spacing=5, test=False):
@@ -183,4 +229,4 @@ class RHBotClient():
 
 
 if __name__ == "__main__":
-    RHBotClient.start(test=True)
+    RHBotClient.start(test=False)
