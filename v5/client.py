3d56d8cc1e10f5dcda12c31d946b273ab0f22d8d
Gavin <gavin.feeney01@gmail.com>
2021-05-03 10:02:40 +0100

    Initial commit towards #10

---
+++ v5/client.py
@@ -0,0 +1,164 @@
+import socket
+import select
+import errno
+import sys
+from pynput.keyboard import Key, Listener, KeyCode
+from pynput import mouse, keyboard
+from random import randint
+import subprocess
+import threading
+import time
+
+
+class RHBotClientConnection():
+    def __init__(self, ip) -> None:
+        self.HEADER_LENGTH = 10
+        self.IP = ip
+        self.PORT = 1351
+        self.my_username = "Testy Test "+str(randint(100, 1000))
+
+        # Create a socket
+        # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+        # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to a given ip and port
+        self.client_socket.connect((self.IP, self.PORT))
+
+        # Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+        self.client_socket.setblocking(False)
+
+        # Prepare username and header and send them
+        # We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+        username = self.my_username.encode('utf-8')
+        username_header = f"{len(username):<{self.HEADER_LENGTH}}".encode(
+            'utf-8')
+        self.client_socket.send(username_header + username)
+
+    def send_message(self, message):
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{self.HEADER_LENGTH}}".encode(
+            'utf-8')
+        self.client_socket.send(message_header + message)
+
+    def send_message_delayed(self, message, delay):
+        start_time = time.time()
+        t = threading.Thread(target=self.delay_thread,
+                             args=(message, delay, start_time))
+        t.start()
+
+    def delay_thread(self, message, delay, start_time):
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{self.HEADER_LENGTH}}".encode(
+            'utf-8')
+        time.sleep(delay - (time.time()-start_time))
+        self.client_socket.send(message_header + message)
+
+    def main_loop(self):
+        while True:
+            # Wait for user to input a message
+            message = input(f'{self.my_username} > ')
+
+            # If message is not empty - send it
+            if message:
+
+                # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+                message = message.encode('utf-8')
+                message_header = f"{len(message):<{self.HEADER_LENGTH}}".encode(
+                    'utf-8')
+                self.client_socket.send(message_header + message)
+
+            try:
+                # Now we want to loop over received messages (there might be more than one) and print them
+                while True:
+
+                    # Receive our "header" containing username length, it's size is defined and constant
+                    username_header = self.client_socket.recv(
+                        self.HEADER_LENGTH)
+                    # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+                    if not len(username_header):
+                        print('Connection closed by the server')
+                        sys.exit()
+                    # Convert header to int value
+                    username_length = int(
+                        username_header.decode('utf-8').strip())
+                    # Receive and decode username
+                    username = self.client_socket.recv(
+                        username_length).decode('utf-8')
+                    # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+                    message_header = self.client_socket.recv(
+                        self.HEADER_LENGTH)
+                    message_length = int(
+                        message_header.decode('utf-8').strip())
+                    message = self.client_socket.recv(
+                        message_length).decode('utf-8')
+                    # Print message
+                    print(f'{username} > {message}')
+
+            except IOError as e:
+                # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+                # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+                # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+                # If we got different error code - something happened
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error: {}'.format(str(e)))
+                    sys.exit()
+                # We just did not receive anything
+                continue
+            except Exception as e:
+                # Any other exception - something happened, exit
+                print('Reading error: '.format(str(e)))
+                sys.exit()
+
+
+class ClientKeypressListener():
+    def __init__(self, list_servers) -> None:
+        self.list_servers = list_servers
+        self.listener = None
+        self.unreleased_keys = []
+
+    def start_keypress_listener(self):
+        if self.listener == None:
+            self.listener = Listener(on_press=self.on_press,
+                                     on_release=self.on_release)
+            self.listener.start()
+
+    def on_press(self, key):
+        if str(key) not in self.unreleased_keys:
+            for server in self.list_servers:
+                server.send_message(str(key)+",down")
+            self.unreleased_keys.append(str(key))
+
+    def on_release(self, key):
+        for server in self.list_servers:
+            server.send_message(str(key)+",up")
+        self.unreleased_keys.remove(str(key))
+
+
+class ClientUtils():
+    def grab_online_servers():
+        output = subprocess.run("arp -a", capture_output=True).stdout.decode()
+        list_ips = []
+        with open("servers.txt", "r") as f:
+            lines = f.readlines()
+            for ip in lines:
+                if ip.strip() in output:
+                    list_ips.append(ip.strip())
+        return list_ips
+
+    def start_server_thread(server):
+        t = threading.Thread(target=server.main_loop(), daemon=True)
+        t.start()
+
+
+if __name__ == "__main__":
+    list_ips = ClientUtils.grab_online_servers()
+    list_servers = []
+    for ip in list_ips:
+        list_servers.append(RHBotClientConnection(ip))
+    ckl = ClientKeypressListener(list_servers)
+    ckl.start_keypress_listener()
+    for server in list_servers:
+        ClientUtils.start_server_thread(server)
+    while True:
+        time.sleep(1)
---
+++ v5/server.py
@@ -0,0 +1,115 @@
+import socket
+import select
+import pydirectinput
+import pyautogui
+import time
+import subprocess
+
+
+class ListenServerTest2():
+    def __init__(self, print_only=False) -> None:
+        self.print_only = print_only
+
+        self.HEADER_LENGTH = 10
+        self.IP = self.grab_current_lan_ip()
+        self.PORT = 1351
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.IP, self.PORT))
+        self.server_socket.listen()
+        self.sockets_list = [self.server_socket]
+        self.clients = {}
+        print(f'Listening for connections on {self.IP}:{self.PORT}...')
+
+    def grab_current_lan_ip(self):
+        output = subprocess.run(
+            "ipconfig", capture_output=True).stdout.decode()
+        _, output = output.split("IPv4 Address. . . . . . . . . . . : 192")
+        output, _ = output.split("Subnet Mask")
+        current_lan_ip = "192" + output.strip()
+        return current_lan_ip
+
+    def convert_pynput_to_pag(self, button):
+        PYNPUT_SPECIAL_CASE_MAP = {
+            'alt_l': 'altleft',
+            'alt_r': 'altright',
+            'alt_gr': 'altright',
+            'caps_lock': 'capslock',
+            'ctrl_l': 'ctrlleft',
+            'ctrl_r': 'ctrlright',
+            'page_down': 'pagedown',
+            'page_up': 'pageup',
+            'shift_l': 'shiftleft',
+            'shift_r': 'shiftright',
+            'num_lock': 'numlock',
+            'print_screen': 'printscreen',
+            'scroll_lock': 'scrolllock',
+        }
+
+        # example: 'Key.F9' should return 'F9', 'w' should return as 'w'
+        cleaned_key = button.replace('Key.', '')
+
+        if cleaned_key in PYNPUT_SPECIAL_CASE_MAP:
+            return PYNPUT_SPECIAL_CASE_MAP[cleaned_key]
+
+        return cleaned_key
+
+    def receive_message(self, client_socket):
+        try:
+            message_header = client_socket.recv(self.HEADER_LENGTH)
+            if not len(message_header):
+                return False
+            message_length = int(message_header.decode('utf-8').strip())
+            return {'header': message_header, 'data': client_socket.recv(message_length)}
+        except:
+            return False
+
+    def start(self):
+        while True:
+            read_sockets, _, exception_sockets = select.select(
+                self.sockets_list, [], self.sockets_list)
+            for notified_socket in read_sockets:
+                if notified_socket == self.server_socket:
+                    client_socket, client_address = self.server_socket.accept()
+                    user = self.receive_message(client_socket)
+                    if user is False:
+                        continue
+                    self.sockets_list.append(client_socket)
+                    self.clients[client_socket] = user
+                    print('Accepted new connection from {}:{}, username: {}'.format(
+                        *client_address, user['data'].decode('utf-8')))
+                # Else existing socket is sending a message
+                else:
+                    message = self.receive_message(notified_socket)
+                    if message is False:
+                        print('Closed connection from: {}'.format(
+                            self.clients[notified_socket]['data'].decode('utf-8')))
+                        self.sockets_list.remove(notified_socket)
+                        del self.clients[notified_socket]
+                        continue
+
+                    if self.print_only:
+                        print(message["data"].decode())
+                    else:
+                        button, direction = str(
+                            message["data"].decode("utf-8")).split(",")
+                        if direction == "down":
+                            key = self.convert_pynput_to_pag(
+                                button.replace("'", ""))
+                            print(key)
+                            pyautogui.keyDown(key)
+                        elif direction == "up":
+                            key = self.convert_pynput_to_pag(
+                                button.replace("'", ""))
+                            pyautogui.keyUp(key)
+
+            for notified_socket in exception_sockets:
+                self.sockets_list.remove(notified_socket)
+                del self.clients[notified_socket]
+
+
+if __name__ == "__main__":
+    lst = ListenServerTest2(print_only=True)
+    lst.start()
