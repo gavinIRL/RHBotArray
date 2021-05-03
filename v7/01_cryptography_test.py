cc9c8a06455a65e794ad67c94852f652012a63a6
Gavin <gavin.feeney01@gmail.com>
2021-05-03 20:17:09 +0100

    Add timed encrpyt test #14

--- v7/01_cryptography_test.py
+++ v7/01_cryptography_test.py
@@ -1,6 +1,15 @@
 from cryptography.fernet import Fernet
+from time import time
 
-key = Fernet.generate_key()
-file = open('key.key', 'wb')  # Open the file as bytes
-file.write(key)  # The key is type bytes
+file = open('key.key', 'rb')  # Open the file as wb to read bytes
+key = file.read()  # The key will be type bytes
 file.close()
+
+test_message = "This is the long enough"
+f = Fernet(key)
+
+start_time = time()
+message = test_message.encode()
+f.encrypt(message)
+end_time = time()
+print(end_time-start_time)
