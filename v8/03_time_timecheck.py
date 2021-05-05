7211bf66f92fb691edb30300dc00471e886dbd05
Gavin <gavin.feeney01@gmail.com>
2021-05-05 12:55:33 +0100

    Add file to check timetaking time

---
+++ v8/03_time_timecheck.py
@@ -0,0 +1,10 @@
+import time
+
+start_time = time.time()
+test_bool = (time.time() - start_time) >= 10
+test_bool = (time.time() - start_time) >= 10
+test_bool = (time.time() - start_time) >= 10
+test_bool = (time.time() - start_time) >= 10
+test_bool = (time.time() - start_time) >= 10
+end_time = time.time()
+print(end_time-start_time)
