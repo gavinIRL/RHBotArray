8067bee2955e2c08bdd44c0dd6edc59580b426c5
Gavin <gavin.feeney01@gmail.com>
2021-05-05 08:27:12 +0100

    Add constants information to click check #16

--- v8/02_click_time_check.py
+++ v8/02_click_time_check.py
@@ -3,6 +3,13 @@ import pyautogui
 import pydirectinput
 import ctypes
 
+MOUSE_LEFTDOWN = 0x0002     # left button down
+MOUSE_LEFTUP = 0x0004       # left button up
+MOUSE_RIGHTDOWN = 0x0008    # right button down
+MOUSE_RIGHTUP = 0x0010      # right button up
+MOUSE_MIDDLEDOWN = 0x0020   # middle button down
+MOUSE_MIDDLEUP = 0x0040     # middle button up
+
 start_time = time.time()
 # pyautogui.moveTo(900, 500, 0.01)
 ctypes.windll.user32.SetCursorPos(30, 60)
