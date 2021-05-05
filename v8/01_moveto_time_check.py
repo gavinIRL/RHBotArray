dfba9438e9895b76dd383ff5f35a41e87d65fb40
Gavin <gavin.feeney01@gmail.com>
2021-05-05 07:51:26 +0100

    FInal moveto time check test file #16

--- v8/01_moveto_time_check.py
+++ v8/01_moveto_time_check.py
@@ -1,5 +1,10 @@
 import time
 import pyautogui
 import pydirectinput
+import ctypes
 
 start_time = time.time()
+# pyautogui.moveTo(900, 500, 0.01)
+ctypes.windll.user32.SetCursorPos(900, 500)
+end_time = time.time()
+print(end_time-start_time)
