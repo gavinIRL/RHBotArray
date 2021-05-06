37c4174aaa42a1107a3ed1800d55f01a3bd42f3d
Gavin <gavin.feeney01@gmail.com>
2021-05-06 22:31:36 +0100

    Initial commit towards #23

---
+++ v9/01_tesseract_test.py
@@ -0,0 +1,3 @@
+# This will be a test file for detecting words in the game window
+# Initial usage is aimed at identifying loot to sell
+# And also for automated levelling by identifying mission accepts, etc.
