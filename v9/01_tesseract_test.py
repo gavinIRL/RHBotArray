22630dc045fa1fedcf8b8d677f4f24e2b5d82cef
Gavin <gavin.feeney01@gmail.com>
2021-05-06 22:52:15 +0100

    Add initial text read code #23

--- v9/01_tesseract_test.py
+++ v9/01_tesseract_test.py
@@ -1,3 +1,10 @@
 # This will be a test file for detecting words in the game window
 # Initial usage is aimed at identifying loot to sell
 # And also for automated levelling by identifying mission accepts, etc.
+import pytesseract
+from PIL import Image
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+print(pytesseract.image_to_string('test.png'))
+# print(pytesseract.image_to_string(Image.open('test.png')))
