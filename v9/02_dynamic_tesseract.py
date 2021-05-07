43a0253b6df4e534536ff02fc66b97830387439e
Gavin <gavin.feeney01@gmail.com>
2021-05-07 20:01:05 +0100

    Add basic text detect printout for #27

--- v9/02_dynamic_tesseract.py
+++ v9/02_dynamic_tesseract.py
@@ -1,55 +1,70 @@
 # This will be a test file for dynamically detecting items
 
+from windowcapture import WindowCapture
+from vision import Vision
+from hsvfilter import HsvFilter
 from pytesseract import Output
 import pytesseract
 from PIL import Image
 import os
 import cv2
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # print(pytesseract.image_to_string('test_ideal.png'))
 # # print(pytesseract.image_to_string(Image.open('test.png')))
 
 # print(pytesseract.image_to_boxes(Image.open('test_ideal.png')))
+with open("gamename.txt") as f:
+    gamename = f.readline()
+wincap = WindowCapture(gamename, [510, 260, 755, 450])
+# initialize the Vision class
+vision_limestone = Vision('xprompt67filtv2.jpg')
+# initialize the trackbar window
+# vision_limestone.init_control_gui()
+hsv_filter = HsvFilter(0, 0, 176, 48, 55, 255, 0, 0, 0, 0)
 
+while(True):
 
-image = cv2.imread("test_ideal_large.png")
+    # get an updated image of the game
+    image = wincap.get_screenshot()
+    # pre-process the image
+    image = vision_limestone.apply_hsv_filter(image, hsv_filter)
+    # display the processed image
 
-# swap color channel ordering from BGR (OpenCV’s default) to RGB (compatible with Tesseract and pytesseract).
-# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
-# we need to convert from BGR to RGB format/mode:
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-results = pytesseract.image_to_data(
-    rgb, output_type=Output.DICT, lang='eng')
-boxresults = pytesseract.image_to_boxes(
-    rgb, output_type=Output.DICT, lang='eng')
-print(results)
-print(boxresults)
+    results = pytesseract.image_to_data(
+        rgb, output_type=Output.DICT, lang='eng')
 
-for i in range(0, len(results["text"])):
-    # extract the bounding box coordinates of the text region from the current result
-    tmp_tl_x = results["left"][i]
-    tmp_tl_y = results["top"][i]
-    tmp_br_x = tmp_tl_x + results["width"][i]
-    tmp_br_y = tmp_tl_y + results["height"][i]
-    tmp_level = results["level"][i]
-    conf = results["conf"][i]
-    text = results["text"][i]
+    for i in range(0, len(results["text"])):
+        # extract the bounding box coordinates of the text region from the current result
+        tmp_tl_x = results["left"][i]
+        tmp_tl_y = results["top"][i]
+        tmp_br_x = tmp_tl_x + results["width"][i]
+        tmp_br_y = tmp_tl_y + results["height"][i]
+        tmp_level = results["level"][i]
+        conf = results["conf"][i]
+        text = results["text"][i]
 
-    if(tmp_level == 5):
-        cv2.putText(image, text, (tmp_tl_x, tmp_tl_y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-        cv2.rectangle(image, (tmp_tl_x, tmp_tl_y),
-                      (tmp_br_x, tmp_br_y), (0, 0, 255), 1)
+        if(tmp_level == 5):
+            cv2.putText(image, text, (tmp_tl_x, tmp_tl_y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            cv2.rectangle(image, (tmp_tl_x, tmp_tl_y),
+                          (tmp_br_x, tmp_br_y), (0, 0, 255), 1)
 
-for j in range(0, len(boxresults["left"])):
-    left = boxresults["left"][j]
-    bottom = boxresults["bottom"][j]
-    right = boxresults["right"][j]
-    top = boxresults["top"][j]
-    # cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 1)
+    #cv2.imshow('Filtered', image)
+    break
+    # debug the loop rate
+    # print('FPS {}'.format(1 / (time() - loop_time)))
+    # loop_time = time()
 
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        break
+print(text)
 
-cv2.imshow("image", image)
-cv2.waitKey(0)
+# print("Done")
+os._exit(1)
