818acf6baf37e2605ade1e1207c0d248df65f682
Gavin <gavin.feeney01@gmail.com>
2021-05-12 08:33:17 +0100

    Add working mouse batch test file #33

--- v10/04_mouse_batch_test.py
+++ v10/04_mouse_batch_test.py
@@ -9,6 +9,8 @@ import cv2
 import pytesseract
 import ctypes
 import pydirectinput
+from win32gui import GetWindowText, GetForegroundWindow
+from client import ClientUtils
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,6 +25,10 @@ class BatchRecordTest():
         self.start_time = 0
         self.batch = ""
         self.pressed_keys = []
+        with open("gamename.txt") as f:
+            self.gamename = f.readline()
+        self.game_wincap = WindowCapture(self.gamename)
+        self.scaling = ClientUtils.get_monitor_scaling()
 
     def start(self):
         self.start_keypress_listener()
@@ -36,6 +42,27 @@ class BatchRecordTest():
                         self.start_time = time.time()
                         # print(self.batch)
                         self.batch = ""
+
+    def start_mouse_listener(self):
+        self.mouse_listener = mouse.Listener(
+            on_click=self.on_click)
+        self.mouse_listener.start()
+
+    def on_click(self, x, y, button, pressed):
+        # Need to then check if the click was in the right window
+        # Do this by checking if window focused
+        if GetWindowText(GetForegroundWindow()) == self.gamename:
+            # when pressed is False, that means it's a release event.
+            # let's listen only to mouse click releases
+            if not pressed:
+                # Need to get the ratio compared to window top left
+                # This will allow common usage on other size monitors
+                # print("x={}, y={}".format(x, y))
+                xratio, yratio = self.convert_click_to_ratio(x, y)
+                # print("xrat={}, yrat={}".format(xratio, yratio))
+                self.batch += str(button) + "|click|" + \
+                    "{:.3f}".format((time.time() - self.start_time)) + \
+                    "|"+"{},{}\n".format(xratio, yratio)
 
     def start_keypress_listener(self):
         if self.listener == None:
@@ -84,6 +111,19 @@ class BatchRecordTest():
                     self.start_time = time.time()
                     # print(self.batch)
                     self.batch = ""
+
+    def convert_click_to_ratio(self, truex, truey):
+        # This will grab the current rectangle coords of game window
+        # and then turn the click values into a ratio of positions
+        # versus the game window
+        self.game_wincap.update_window_position(border=False)
+        # Turn the screen pos into window pos
+        relx = (truex - self.game_wincap.window_rect[0]) * self.scaling
+        rely = (truey - self.game_wincap.window_rect[1]) * self.scaling
+        # Then convert to a ratio
+        ratx = relx/(self.game_wincap.w*self.scaling)
+        raty = rely/(self.game_wincap.h*self.scaling)
+        return ratx, raty
 
 
 if __name__ == "__main__":
