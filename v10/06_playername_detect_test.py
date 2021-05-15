5dc5f35caf34a5481beb17e563265504233d6582
Gavin <gavin.feeney01@gmail.com>
2021-05-15 11:21:13 +0100

    Finished test of player name for #45

--- v10/06_playername_detect_test.py
+++ v10/06_playername_detect_test.py
@@ -17,10 +17,21 @@ class DetectNameTest():
     def __init__(self) -> None:
         with open("gamename.txt") as f:
             self.gamename = f.readline()
-        self.game_wincap = WindowCapture(self.gamename)
+        plyrname_rect = [165, 45, 320, 65]
+        self.plyrname_wincap = WindowCapture(self.gamename, plyrname_rect)
+        self.plyrname_filt = HsvFilter(0, 0, 103, 89, 104, 255, 0, 0, 0, 0)
+        self.plyrmname_vision = Vision('xprompt67filtv2.jpg')
 
     def detect_name(self):
-        pass
+        # get an updated image of the game
+        image = self.plyrname_wincap.get_screenshot()
+        # pre-process the image
+        image = self.plyrmname_vision.apply_hsv_filter(
+            image, self.plyrname_filt)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pytesseract.image_to_data(
+            rgb, output_type=pytesseract.Output.DICT, lang='eng')
+        print(results["text"][4])
 
 
 if __name__ == "__main__":
