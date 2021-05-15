eca8f7a64f65dea36588492be17381422debe617
Gavin <gavin.feeney01@gmail.com>
2021-05-15 22:20:52 +0100

    Add final pixel values to grab for rgb interr #30

--- v10/10_coloured_loot_test.py
+++ v10/10_coloured_loot_test.py
@@ -14,16 +14,20 @@ with open("gamename.txt") as f:
     gamename = f.readline()
 game_wincap = WindowCapture(gamename)
 
+# Options for x and y values
+# x = 512, 556, 600, 645, 689, 733
+# y = 277, 321, 365, 411
+
 # The next block of code is for detecting the object in question
 # initialize the WindowCapture class for object detection
-object_wincap = WindowCapture(gamename, [510, 250, 775, 430])
+object_wincap = WindowCapture(gamename, [688, 411, 775, 430])
 # initialize the Vision class
 object_vision = Vision('emptyslot67filt.jpg')
 screenshot = object_wincap.get_screenshot()
 print(screenshot[0, 0])
 while(True):
     cv.imshow('Matches', screenshot)
-    sleep(0.15)
+    sleep(0.1)
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
         break
