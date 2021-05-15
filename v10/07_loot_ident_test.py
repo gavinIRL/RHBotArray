4fdb8a410086744ff089c662535ca33502bd56c9
Gavin <gavin.feeney01@gmail.com>
2021-05-15 13:44:02 +0100

    Add initial test file for #30

---
+++ v10/07_loot_ident_test.py
@@ -0,0 +1,26 @@
+import time
+import os
+from pynput.keyboard import Key, Listener, KeyCode
+from pynput import mouse, keyboard
+from hsvfilter import HsvFilter
+from windowcapture import WindowCapture
+from vision import Vision
+import cv2
+import pytesseract
+import ctypes
+import pydirectinput
+from win32gui import GetWindowText, GetForegroundWindow
+from client import ClientUtils
+
+
+class LootIdentTest():
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+
+if __name__ == "__main__":
+    lit = LootIdentTest()
+    lit.start()
