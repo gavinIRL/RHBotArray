91bb651896b2d0d0f0b584cff72336fb9a5bb747
Gavin <gavin.feeney01@gmail.com>
2021-05-16 13:18:50 +0100

    Add rarity ident based on colour #30

--- v10/10_coloured_loot_test.py
+++ v10/10_coloured_loot_test.py
@@ -15,16 +15,35 @@ with open("gamename.txt") as f:
 game_wincap = WindowCapture(gamename)
 
 # Options for x and y values
-# x = 512, 556, 600, 645, 689, 733
+# x = 512, 556, 600, 644, 684, 732
 # y = 277, 321, 365, 411
 
 # The next block of code is for detecting the object in question
 # initialize the WindowCapture class for object detection
-object_wincap = WindowCapture(gamename, [688, 411, 775, 430])
+object_wincap = WindowCapture(gamename, [512, 277, 775, 430])
 # initialize the Vision class
 object_vision = Vision('emptyslot67filt.jpg')
 screenshot = object_wincap.get_screenshot()
-print(screenshot[0, 0])
+start = time()
+data = []
+for i in range(4):
+    for j in range(6):
+        colour = screenshot[i*44, j*44]
+        data.append([i+1, j+1, colour[0], colour[1], colour[2]])
+for line in data:
+    rgb = "{},{},{}".format(line[2], line[3], line[4])
+    if rgb == "24,33,48":
+        rarity = "common"
+    elif rgb == "2,204,43":
+        rarity = "green"
+    elif rgb == "232,144,5":
+        rarity = "blue"
+    else:
+        rarity = rgb
+    print("row: {}, col: {}, Rarity={}".format(
+        line[0], line[1], rarity))
+end = time()
+print(end-start)
 while(True):
     cv.imshow('Matches', screenshot)
     sleep(0.1)
