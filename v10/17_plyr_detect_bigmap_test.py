e1f17e0dc95bde413fa1e280d5d48e2ee7f17922
Gavin <gavin.feeney01@gmail.com>
2021-05-18 20:19:44 +0100

    Found a more accurate bigmap player detect #55

--- v10/17_plyr_detect_bigmap_test.py
+++ v10/17_plyr_detect_bigmap_test.py
@@ -2,6 +2,7 @@ import cv2 as cv
 from hsvfilter import HsvFilter, grab_object_preset
 import numpy as np
 import os
+from vision import Vision
 
 
 class BigMapPlayerCheck():
@@ -18,7 +19,7 @@ class BigMapPlayerCheck():
             c[c > lim] -= amount
         return c
 
-    def save_and_convert(self, filter, existing_image, save_image):
+    def filter_blackwhite_invert(self, filter, existing_image):
         img = cv.imread(existing_image, cv.IMREAD_UNCHANGED)
         hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
         hsv_filter = filter
@@ -43,15 +44,26 @@ class BigMapPlayerCheck():
         grayImage = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # now change it to black and white
         (thresh, blackAndWhiteImage) = cv.threshold(
-            grayImage, 127, 255, cv.THRESH_BINARY)
+            grayImage, 67, 255, cv.THRESH_BINARY)
         # now invert it
         inverted = (255-blackAndWhiteImage)
-        cv.imwrite(save_image, inverted)
+        inverted = cv.cvtColor(inverted, cv.COLOR_GRAY2BGR)
+        return inverted
+        # cv.imwrite(save_image, inverted)
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 filter = HsvFilter(34, 160, 122, 50, 255, 255, 0, 0, 0, 0)
 sfi = BigMapPlayerCheck()
-existing_image = "SW.png"
-save_image = "SW_plyronly.jpg"
-sfi.save_and_convert(filter, existing_image, save_image)
+existing_image = "CH.jpg"
+save_image = sfi.filter_blackwhite_invert(filter, existing_image)
+existing_image = cv.imread(existing_image)
+
+vision_limestone = Vision('plyr.jpg')
+# save_image = vision_limestone.apply_hsv_filter(save_image)
+rectangles = vision_limestone.find(
+    save_image, threshold=0.31, epsilon=0.5)
+output_image = vision_limestone.draw_rectangles(save_image, rectangles)
+cv.imshow('Matches', output_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
