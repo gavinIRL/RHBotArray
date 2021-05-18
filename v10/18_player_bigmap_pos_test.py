ad74c4a3b8f094c1599a4916c92ab79dc98154bc
Gavin <gavin.feeney01@gmail.com>
2021-05-18 20:24:13 +0100

    Add test for bigmap plyr detect #54 #55

---
+++ v10/18_player_bigmap_pos_test.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+from hsvfilter import HsvFilter, grab_object_preset
+import numpy as np
+import os
+from vision import Vision
+import time
+import time
+import os
+from pynput.keyboard import Key, Listener, KeyCode
+from pynput import mouse, keyboard
+from hsvfilter import HsvFilter
+from windowcapture import WindowCapture
+from vision import Vision
+import cv2
+import pytesseract
+import ctypes
+import pydirectinput
+from win32gui import GetWindowText, GetForegroundWindow
+from client import ClientUtils
+
+
+class PlayerPositionTest():
+
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+    def detect_level_name(self):
+        pass
+
+    def detect_bigmap_open(self):
+        pass
