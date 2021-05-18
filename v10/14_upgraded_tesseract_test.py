38cd0a21dd1fe783d4941d527ed5a615c5f964f2
Gavin <gavin.feeney01@gmail.com>
2021-05-18 10:18:34 +0100

    Add an upgraded test file for #54

---
+++ v10/14_upgraded_tesseract_test.py
@@ -0,0 +1,42 @@
+import time
+import os
+from pynput.keyboard import Key, Listener, KeyCode
+from pynput import mouse, keyboard
+from hsvfilter import HsvFilter
+from windowcapture import WindowCapture
+from vision import Vision
+import cv2
+import pytesseract
+import ctypes
+import pydirectinput
+from win32gui import GetWindowText, GetForegroundWindow
+from client import ClientUtils
+
+
+class UpgradedDetectTest():
+    def __init__(self) -> None:
+        with open("gamename.txt") as f:
+            self.gamename = f.readline()
+        plyrname_rect = [165, 45, 320, 65]
+        self.plyrname_wincap = WindowCapture(self.gamename, plyrname_rect)
+        self.plyrname_filt = HsvFilter(0, 0, 103, 89, 104, 255, 0, 0, 0, 0)
+        self.plyrmname_vision = Vision('xprompt67filtv2.jpg')
+
+    def detect_name(self):
+        # get an updated image of the game
+        image = self.plyrname_wincap.get_screenshot()
+        # pre-process the image
+        image = self.plyrmname_vision.apply_hsv_filter(
+            image, self.plyrname_filt)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # results = pytesseract.image_to_data(
+        #     rgb, output_type=pytesseract.Output.DICT, lang='eng')
+        # print(results["text"][4])
+        result = pytesseract.image_to_string(
+            rgb, lang='eng', boxes=False, config='--psm 7 --oem 3 -c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+        print(result)
+
+
+if __name__ == "__main__":
+    udt = UpgradedDetectTest
+    udt.detect_name()
