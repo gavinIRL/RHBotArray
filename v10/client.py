c911fc93905691dc128ae9f1f5bc61850f85e000
Gavin <gavin.feeney01@gmail.com>
2021-05-22 18:07:01 +0100

    Remove unnecessary test 21 code from client

--- v10/client.py
+++ v10/client.py
@@ -124,7 +124,6 @@ class ClientKeypressListener():
         self.speeds = {}
         self.num_names = []
         self.load_level_rects()
-        self.key_map = ClientUtils.load_key_dict()
         self.player_pos = None
 
     def try_toggle_map(self):
@@ -564,51 +563,6 @@ class ClientUtils():
         user32.SetProcessDPIAware()
         [w, h] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
         return float(("{:.2f}".format(w/w_orig)))
-
-    def load_key_dict(self):
-        KEYBOARD_MAPPING = {
-            '1': 0x02,
-            '2': 0x03,
-            '3': 0x04,
-            '4': 0x05,
-            '5': 0x06,
-            '6': 0x07,
-            '7': 0x08,
-            '8': 0x09,
-            '9': 0x0A,
-            '0': 0x0B,
-            'q': 0x10,
-            'w': 0x11,
-            'e': 0x12,
-            'r': 0x13,
-            't': 0x14,
-            'y': 0x15,
-            'u': 0x16,
-            'i': 0x17,
-            'o': 0x18,
-            'p': 0x19,
-            'a': 0x1E,
-            's': 0x1F,
-            'd': 0x20,
-            'f': 0x21,
-            'g': 0x22,
-            'h': 0x23,
-            'j': 0x24,
-            'k': 0x25,
-            'l': 0x26,
-            'z': 0x2C,
-            'x': 0x2D,
-            'c': 0x2E,
-            'v': 0x2F,
-            'b': 0x30,
-            'n': 0x31,
-            'm': 0x32,
-            'up': MapVirtualKey(0x26, MAPVK_VK_TO_VSC),
-            'left': MapVirtualKey(0x25, MAPVK_VK_TO_VSC),
-            'down': MapVirtualKey(0x28, MAPVK_VK_TO_VSC),
-            'right': MapVirtualKey(0x27, MAPVK_VK_TO_VSC),
-        }
-        return KEYBOARD_MAPPING
 
 
 class RHBotClient():
