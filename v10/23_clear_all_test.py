4922a4f320f1ea22e3f4382170ebc452ddaec084
Gavin <gavin.feeney01@gmail.com>
2021-05-23 15:31:56 +0100

    Initial commit towards #69

---
+++ v10/23_clear_all_test.py
@@ -0,0 +1,13 @@
+
+
+class ClearAllTest():
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+
+if __name__ == "__main__":
+    cat = ClearAllTest()
+    cat.start()
