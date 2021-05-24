92fd36e66bcce16bb64e2fbe30e71725a3215687
Gavin <gavin.feeney01@gmail.com>
2021-05-24 17:47:13 +0100

    Add initial listener and loop for #53

--- Helper/helper.py
+++ Helper/helper.py
@@ -2,13 +2,29 @@
 # 1 - ClearAll
 # 2 - Questhandle
 # 3 - Sell & Repair
+from pynput.keyboard import Key, Listener, KeyCode
+import time
+
 
 class Helper():
     def __init__(self) -> None:
-        pass
+        self.listener = None
+        self.unreleased_keys = []
 
     def start(self):
-        pass
+        self.start_keypress_listener()
+        while True:
+            time.sleep(0.5)
+
+    def start_keypress_listener(self):
+        if self.listener == None:
+            if not self.test:
+                self.listener = Listener(on_press=self.on_press,
+                                         on_release=self.on_release)
+            else:
+                self.listener = Listener(on_press=self.on_press_test,
+                                         on_release=self.on_release_test)
+            self.listener.start()
 
 
 if __name__ == "__main__":
