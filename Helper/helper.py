1c7ff6cc6700494f7a63c8d36281461878aceb35
Gavin <gavin.feeney01@gmail.com>
2021-05-24 15:51:33 +0100

    Add initial file structure for #53

---
+++ Helper/helper.py
@@ -0,0 +1,16 @@
+# This file needs to include the following:
+# 1 - ClearAll
+# 2 - Questhandle
+# 3 - Sell & Repair
+
+class Helper():
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+
+if __name__ == "__main__":
+    h = Helper()
+    h.start()
