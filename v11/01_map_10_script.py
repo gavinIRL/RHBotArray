c043641416c747266822331cc1dbb59ec20965c1
Gavin <gavin.feeney01@gmail.com>
2021-05-25 18:54:29 +0100

    Rename map 10 script file for #73

---
+++ v11/01_map_10_script.py
@@ -0,0 +1,29 @@
+# This file will automatically run through map 10
+
+class Map10_MS30():
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+    def move_to(self, x, y, angle):
+        pass
+
+    def roomclear_skill(self):
+        pass
+
+    def continue_clear(self):
+        pass
+
+    def ident_farloot(self):
+        pass
+
+    def ident_nearloot(self):
+        pass
+
+    def boss_combo_1(self):
+        pass
+
+    def boss_combo_2(self):
+        pass
