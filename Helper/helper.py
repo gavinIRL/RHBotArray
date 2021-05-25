6844b6bb285a9563e6d5aeb75720e89bd9aa652a
Gavin <gavin.feeney01@gmail.com>
2021-05-25 06:53:09 +0100

    Add rest of keypress handling for #53

--- Helper/helper.py
+++ Helper/helper.py
@@ -4,6 +4,7 @@
 # 3 - Sell & Repair
 from pynput.keyboard import Key, Listener, KeyCode
 import time
+import os
 
 
 class Helper():
@@ -18,13 +19,23 @@ class Helper():
 
     def start_keypress_listener(self):
         if self.listener == None:
-            if not self.test:
-                self.listener = Listener(on_press=self.on_press,
-                                         on_release=self.on_release)
-            else:
-                self.listener = Listener(on_press=self.on_press_test,
-                                         on_release=self.on_release_test)
+            self.listener = Listener(on_press=self.on_press,
+                                     on_release=self.on_release)
             self.listener.start()
+
+    def on_press(self, key):
+        if key == KeyCode(char='0'):
+            print("Exiting bot")
+            os._exit(1)
+        elif key == KeyCode(char='7'):
+            os.popen('python sell_repair.py')
+        elif key == KeyCode(char='8'):
+            self.quest_handle.start_quest_handle()
+        elif key == KeyCode(char='-'):
+            self.clear_all()
+
+    def on_release(self, key):
+        pass
 
 
 if __name__ == "__main__":
