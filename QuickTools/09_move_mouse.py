e48bfa3049de5b538b0e458754f9bec4f32ae88a
Gavin <gavin.feeney01@gmail.com>
2021-05-31 20:12:55 +0100

    Add util file to move mouse

---
+++ QuickTools/09_move_mouse.py
@@ -0,0 +1,13 @@
+# This file will move the mouse to a particular location
+
+class MoveMouse():
+    def __init__(self, game=False) -> None:
+        pass
+
+    def move_mouse(self, x, y, rat=False):
+        pass
+
+
+if __name__ == "__main__":
+    mm = MoveMouse()
+    mm.move_mouse(100, 200)
