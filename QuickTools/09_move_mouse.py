1f83717484faa150251c67c3faa335507dc568b3
Gavin <gavin.feeney01@gmail.com>
2021-05-31 20:23:58 +0100

    Add code to move mouse to position

--- QuickTools/09_move_mouse.py
+++ QuickTools/09_move_mouse.py
@@ -1,11 +1,39 @@
 # This file will move the mouse to a particular location
+from windowcapture import WindowCapture
+import ctypes
+
 
 class MoveMouse():
     def __init__(self, game=False) -> None:
-        pass
+        with open("gamename.txt") as f:
+            self.gamename = f.readline()
+        self.game = game
+        if game:
+            self.game_wincap = WindowCapture(self.gamename)
 
     def move_mouse(self, x, y, rat=False):
-        pass
+        if rat:
+            x, y = self.convert_ratio_to_click(x, y)
+            ctypes.windll.user32.SetCursorPos(x, y)
+        elif self.game:
+            x = x + self.wincap.window_rect[0]
+            y = y + self.wincap.window_rect[1]
+            ctypes.windll.user32.SetCursorPos(x, y)
+        else:
+            ctypes.windll.user32.SetCursorPos(x, y)
+
+    def convert_ratio_to_click(self, ratx, raty):
+        # This will grab the current rectangle coords of game window
+        # and then turn the ratio of positions versus the game window
+        # into true x,y coords
+        self.wincap.update_window_position(border=False)
+        # Turn the ratios into relative
+        relx = int(ratx * self.wincap.w)
+        rely = int(raty * self.wincap.h)
+        # Turn the relative into true
+        truex = int((relx + self.wincap.window_rect[0]))
+        truey = int((rely + self.wincap.window_rect[1]))
+        return truex, truey
 
 
 if __name__ == "__main__":
