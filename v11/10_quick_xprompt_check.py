f17e6db727aebd1451fcf7b2552746e500b83d31
Gavin <gavin.feeney01@gmail.com>
2021-06-02 17:10:02 +0100

    Add working test file for #90

--- v11/10_quick_xprompt_check.py
+++ v11/10_quick_xprompt_check.py
@@ -10,18 +10,18 @@ def xprompt_checker():
     with open("gamename.txt") as f:
         gamename = f.readline()
     wincap = WindowCapture(gamename, custom_rect=[
-        1135, 691, 1162, 692])
+        1137, 694, 1163, 752])
     # while True:
     image = wincap.get_screenshot()
     a, b, c = [int(i) for i in image[0][0]]
     d, e, f = [int(i) for i in image[0][-1]]
     print("abc: {},{},{}".format(a, b, c))
     print("def: {},{},{}".format(d, e, f))
-    # if a+b+c > 700 and d+e+f > 700:
-    #     print("Detected sect clear")
-    #     break
-    # else:
-    #     time.sleep(0.1)
+    cv2.imwrite("testytest.jpg", image)
+    if a+b+d+e > 960 and c+f == 70:
+        return True
+    else:
+        return False
 
 
 xprompt_checker()
