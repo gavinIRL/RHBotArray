5ab23bdf3d4f24db7129d8033669091401c7031e
Gavin <gavin.feeney01@gmail.com>
2021-06-03 21:35:25 +0100

    Initial commit for looting current room #93

---
+++ v11/13_loot_cur_room_test.py
@@ -0,0 +1,4 @@
+# This file will test looting of the current room
+# It will require room details such as loot search points
+# and automatically progress through each position
+# and loot everything it can find
