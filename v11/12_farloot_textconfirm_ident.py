a05127f37fc8f477105db19d65212e0c5a29f0e5
Gavin <gavin.feeney01@gmail.com>
2021-06-03 17:45:34 +0100

    Add initial file for #91

---
+++ v11/12_farloot_textconfirm_ident.py
@@ -0,0 +1,7 @@
+import pydirectinput
+from rhba_utils import BotUtils
+from custom_input import CustomInput
+import time
+import os
+import math
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
