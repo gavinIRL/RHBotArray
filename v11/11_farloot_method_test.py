74c515d062fb55b11d301834df49e8b8d6e72273
Gavin <gavin.feeney01@gmail.com>
2021-06-03 13:29:18 +0100

    Add initial steps in farloot method #89

--- v11/11_farloot_method_test.py
+++ v11/11_farloot_method_test.py
@@ -7,4 +7,20 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def loot_nearest_item(gamename, player_name):
-    pass
+    # First need to close anything that might be in the way
+    BotUtils.close_map_and_menu()
+    # Then grab loot locations
+    loot_list = BotUtils.grab_farloot_locationsv2(gamename)
+    # If none then return False
+    if not loot_list:
+        return False
+    # Then grab the player location
+    playerx, playery = BotUtils.grab_character_location(
+        player_name, gamename)
+    # If didn't find player then try once more
+    if not playerx:
+        playerx, playery = BotUtils.grab_character_location(
+            player_name, gamename)
+        if not playerx:
+            return False
+    # Then convert lootlist to rel_pos list and find closest
