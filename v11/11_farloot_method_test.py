998ce78ad138f70671e21d64723e55d65d89c4b4
Gavin <gavin.feeney01@gmail.com>
2021-06-03 13:10:24 +0100

    Add test file to create robust farloot nav #89

---
+++ v11/11_farloot_method_test.py
@@ -0,0 +1,10 @@
+from rhba_utils import BotUtils
+from custom_input import CustomInput
+import time
+import os
+import math
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def loot_nearest_item(gamename, player_name):
+    pass
