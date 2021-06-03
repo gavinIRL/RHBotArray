12673431a9bd9dbbfa85e6e5e81cefd620784fa7
Gavin <gavin.feeney01@gmail.com>
2021-06-03 17:57:43 +0100

    Add a testprint and timer, fix rectsize #91

--- v11/12_farloot_textconfirm_ident.py
+++ v11/12_farloot_textconfirm_ident.py
@@ -13,16 +13,22 @@ def confirm_loot_test(gamename):
     BotUtils.close_map_and_menu(gamename)
     # Then grab loot locations
     loot_list = BotUtils.grab_farloot_locationsv2(gamename)
+    confirmed_list = []
     for index, coords in enumerate(loot_list):
+        start_time = time.time()
         x, y = coords
-        wincap = WindowCapture(gamename, [x-70, y+15, x+70, y+15])
+        wincap = WindowCapture(gamename, [x-70, y-15, x+70, y+15])
         rgb = wincap.get_screenshot()
         tess_config = '--psm 7 --oem 3 -c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
         result = pytesseract.image_to_string(
             rgb, lang='eng', config=tess_config)[:-2]
-        print("Index:{}, result={}".format(index, result))
+        # print("Index:{}, result={}".format(index, result))
+        if len(result) > 3:
+            confirmed_list.append(loot_list[index])
+        print("Time taken for confirmation: {}s".format(time.time()-start_time))
 
 
+time.sleep(2)
 with open("gamename.txt") as f:
     gamename = f.readline()
 confirm_loot_test(gamename)
