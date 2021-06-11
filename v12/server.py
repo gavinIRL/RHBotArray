fc0ed161ea259ca48c5dcb39baf4ffd6f3d2fbb0
Gavin <gavin.feeney01@gmail.com>
2021-06-11 14:56:43 +0100

    Add updated regroup, start change to custom #98

--- v12/server.py
+++ v12/server.py
@@ -166,7 +166,7 @@ class RHBotArrayServer():
 
     def try_toggle_map(self):
         # pydirectinput.keyDown("m")
-        self.press_key(self.key_map["m"])
+        CustomInput.press_key(CustomInput.key_map["m"])
         time.sleep(0.05)
         # pydirectinput.keyUp("m")
         self.release_key(self.key_map["m"])
@@ -331,7 +331,7 @@ class RHBotArrayServer():
                 key = "up"
         if abs(dist) > 5:
             # pydirectinput.keyDown(key)
-            self.press_key(self.key_map[key], key)
+            CustomInput.press_key(CustomInput.key_map[key], key)
 
     def resolve_direction(self, dir):
         if dir == "x":
@@ -368,13 +368,13 @@ class RHBotArrayServer():
                 # Need to reverse direction
                 if first_rel_dists[index] > 0:
                     # pydirectinput.keyDown(key1)
-                    self.press_key(self.key_map[key1], key1)
+                    CustomInput.press_key(CustomInput.key_map[key1], key1)
                     time.sleep((end_time)/percent_moved)
                     # pydirectinput.keyUp(key1)
                     self.release_key(self.key_map[key1], key1)
                 else:
                     # pydirectinput.keyDown(key2)
-                    self.press_key(self.key_map[key2], key2)
+                    CustomInput.press_key(CustomInput.key_map[key2], key2)
                     time.sleep((end_time)/percent_moved)
                     # pydirectinput.keyUp(key2)
                     self.release_key(self.key_map[key2], key2)
@@ -607,7 +607,7 @@ class RHBotArrayServer():
                     else:
                         self.release_key(self.key_map["x"])
             elif button == "regroup":
-                self.regroupv2(direction)
+                self.regroup(direction)
             elif button == "autoloot":
                 if direction == "on":
                     self.autoloot_thread_start()
@@ -623,7 +623,7 @@ class RHBotArrayServer():
             elif button == "batch":
                 self.batch_handle(direction)
             elif button == "clearall":
-                self.clear_all()
+                BotUtils.close_map_and_menu(self.gamename)
             elif button == "sellrepair":
                 os.popen('python sell_repair.py')
             elif button == "mainplayer":
@@ -662,176 +662,6 @@ class RHBotArrayServer():
                         pydirectinput.keyUp(key)
                     else:
                         self.release_key(self.key_map[key], key)
-
-    def load_key_dict(self):
-        KEYBOARD_MAPPING = {
-            'escape': 0x01,
-            'esc': 0x01,
-            'f1': 0x3B,
-            'f2': 0x3C,
-            'f3': 0x3D,
-            'f4': 0x3E,
-            'f5': 0x3F,
-            'f6': 0x40,
-            'f7': 0x41,
-            'f8': 0x42,
-            'f9': 0x43,
-            'f10': 0x44,
-            'f11': 0x57,
-            'f12': 0x58,
-            'printscreen': 0xB7,
-            'prntscrn': 0xB7,
-            'prtsc': 0xB7,
-            'prtscr': 0xB7,
-            'scrolllock': 0x46,
-            'pause': 0xC5,
-            '`': 0x29,
-            '1': 0x02,
-            '2': 0x03,
-            '3': 0x04,
-            '4': 0x05,
-            '5': 0x06,
-            '6': 0x07,
-            '7': 0x08,
-            '8': 0x09,
-            '9': 0x0A,
-            '0': 0x0B,
-            '-': 0x0C,
-            '=': 0x0D,
-            'backspace': 0x0E,
-            'insert': 0xD2 + 1024,
-            'home': 0xC7 + 1024,
-            'pageup': 0xC9 + 1024,
-            'pagedown': 0xD1 + 1024,
-            # numpad
-            'numlock': 0x45,
-            'divide': 0xB5 + 1024,
-            'multiply': 0x37,
-            'subtract': 0x4A,
-            'add': 0x4E,
-            'decimal': 0x53,
-            'numpadenter': 0x9C + 1024,
-            'numpad1': 0x4F,
-            'numpad2': 0x50,
-            'numpad3': 0x51,
-            'numpad4': 0x4B,
-            'numpad5': 0x4C,
-            'numpad6': 0x4D,
-            'numpad7': 0x47,
-            'numpad8': 0x48,
-            'numpad9': 0x49,
-            'numpad0': 0x52,
-            # end numpad
-            'tab': 0x0F,
-            'q': 0x10,
-            'w': 0x11,
-            'e': 0x12,
-            'r': 0x13,
-            't': 0x14,
-            'y': 0x15,
-            'u': 0x16,
-            'i': 0x17,
-            'o': 0x18,
-            'p': 0x19,
-            '[': 0x1A,
-            ']': 0x1B,
-            '\\': 0x2B,
-            'del': 0xD3 + 1024,
-            'delete': 0xD3 + 1024,
-            'end': 0xCF + 1024,
-            'capslock': 0x3A,
-            'a': 0x1E,
-            's': 0x1F,
-            'd': 0x20,
-            'f': 0x21,
-            'g': 0x22,
-            'h': 0x23,
-            'j': 0x24,
-            'k': 0x25,
-            'l': 0x26,
-            ';': 0x27,
-            "'": 0x28,
-            'enter': 0x1C,
-            'return': 0x1C,
-            'shift': 0x2A,
-            'shiftleft': 0x2A,
-            'z': 0x2C,
-            'x': 0x2D,
-            'c': 0x2E,
-            'v': 0x2F,
-            'b': 0x30,
-            'n': 0x31,
-            'm': 0x32,
-            ',': 0x33,
-            '.': 0x34,
-            '/': 0x35,
-            'shiftright': 0x36,
-            'ctrl': 0x1D,
-            'ctrlleft': 0x1D,
-            'win': 0xDB + 1024,
-            'winleft': 0xDB + 1024,
-            'alt': 0x38,
-            'altleft': 0x38,
-            ' ': 0x39,
-            'space': 0x39,
-            'altright': 0xB8 + 1024,
-            'winright': 0xDC + 1024,
-            'apps': 0xDD + 1024,
-            'ctrlright': 0x9D + 1024,
-            'up': MapVirtualKey(0x26, MAPVK_VK_TO_VSC),
-            'left': MapVirtualKey(0x25, MAPVK_VK_TO_VSC),
-            'down': MapVirtualKey(0x28, MAPVK_VK_TO_VSC),
-            'right': MapVirtualKey(0x27, MAPVK_VK_TO_VSC),
-        }
-        return KEYBOARD_MAPPING
-
-    def press_key(self, hexKeyCode, key="T"):
-        if key in ["up", "down", "left", "right"]:
-            # Do the primary key
-            hexKeyCode2 = 0xE0
-            extra = ctypes.c_ulong(0)
-            ii_ = Input_I()
-            ii_.ki = KeyBdInput(0, hexKeyCode2, 0x0008,
-                                0, ctypes.pointer(extra))
-            x = Input(ctypes.c_ulong(1), ii_)
-            SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-            # then the arrow itself
-            ii_.ki = KeyBdInput(0, hexKeyCode, 0x0001,
-                                0, ctypes.pointer(extra))
-            x = Input(ctypes.c_ulong(1), ii_)
-            ctypes.windll.user32.SendInput(
-                1, ctypes.pointer(x), ctypes.sizeof(x))
-            SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-        else:
-            extra = ctypes.c_ulong(0)
-            ii_ = Input_I()
-            ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008,
-                                0, ctypes.pointer(extra))
-            x = Input(ctypes.c_ulong(1), ii_)
-            ctypes.windll.user32.SendInput(
-                1, ctypes.pointer(x), ctypes.sizeof(x))
-
-    def release_key(self, hexKeyCode, key="T"):
-        keybdFlags = 0x0008 | 0x0002
-        if key in ["up", "down", "left", "right"]:
-            keybdFlags |= 0x0001
-        extra = ctypes.c_ulong(0)
-        ii_ = Input_I()
-        ii_.ki = KeyBdInput(0, hexKeyCode, keybdFlags,
-                            0, ctypes.pointer(extra))
-        x = Input(ctypes.c_ulong(1), ii_)
-        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-        if key in ["up", "down", "left", "right"] and ctypes.windll.user32.GetKeyState(0x90):
-            hexKeyCode = 0xE0
-            extra = ctypes.c_ulong(0)
-            ii_ = Input_I()
-            ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002,
-                                0, ctypes.pointer(extra))
-            x = Input(ctypes.c_ulong(1), ii_)
-            ctypes.windll.user32.SendInput(
-                1, ctypes.pointer(x), ctypes.sizeof(x))
 
     def filter_blackwhite_invert(self, filter, existing_image):
         hsv = cv2.cvtColor(existing_image, cv2.COLOR_BGR2HSV)
@@ -951,7 +781,7 @@ class RHBotArrayServer():
             self.try_toggle_map()
         self.player_pos = self.grab_player_pos()
 
-    def regroupv2(self, coords: str):
+    def regroup(self, coords: str):
         x, y = coords.split("|")
         # first perform the pre-regroup updates
         self.pre_regroup_updates()
@@ -959,71 +789,20 @@ class RHBotArrayServer():
         try:
             relx = self.player_pos[0] - int(x)
             rely = int(y) - self.player_pos[1]
-            # First take care of the x-dir
-            if relx != 0:
-                if relx > 150:
-                    raise Exception
-                else:
-                    self.resolve_dir_v2(relx, "x")
-            if rely != 0:
-                if rely > 150:
-                    raise Exception
-                else:
-                    self.resolve_dir_v2(rely, "y")
+            BotUtils.move_diagonal(self.gamename, relx,
+                                   rely, 90, self.speed, True)
         except:
             if self.regroup_try_count < 2:
                 self.regroup_try_count += 1
-                pydirectinput.keyDown("right")
-                time.sleep(0.01)
-                pydirectinput.keyUp("right")
-                self.regroupv2(coords)
+                key = "right"
+                CustomInput.press_key(CustomInput.key_map[key])
+                CustomInput.release_key(CustomInput.key_map[key])
+                self.regroup(coords)
         # Finally close the map
         # if self.detect_bigmap_open():
         #     self.try_toggle_map()
         self.player_pos = [0, 0]
         self.regroup_try_count = 0
-
-    def resolve_dir_v2(self, value, dir):
-        if dir == "x":
-            if value > 0:
-                key = "left"
-            else:
-                key = "right"
-        elif dir == "y":
-            if value > 0:
-                key = "down"
-            else:
-                key = "up"
-        time_reqd = abs(value/self.speed)
-        self.press_key(self.key_map[key], key)
-        time.sleep(time_reqd-0.003)
-        self.release_key(self.key_map[key], key)
-
-    def clear_all(self):
-        if self.detect_menu_open():
-            self.close_esc_menu()
-        elif self.detect_bigmap_open():
-            self.close_map()
-
-    def detect_menu_open(self):
-        wincap = WindowCapture(self.gamename, custom_rect=[595, 278, 621, 479])
-        image = wincap.get_screenshot()
-        cv2.imwrite("testy.jpg", image)
-        a, b, c = [int(i) for i in image[0][0]]
-        d, e, f = [int(i) for i in image[0][-1]]
-        # print("Sum abc:{}, def:{}".format(a+b+c, d+e+f))
-        if a+b+c > 700:
-            if d+e+f > 700:
-                return True
-        return False
-
-    def close_map(self):
-        pydirectinput.click(
-            int(self.scaling*859+self.game_wincap.window_rect[0]), int(self.scaling*260+self.game_wincap.window_rect[1]))
-
-    def close_esc_menu(self):
-        pydirectinput.click(
-            int(self.scaling*749+self.game_wincap.window_rect[0]), int(self.scaling*280+self.game_wincap.window_rect[1]))
 
     def create_follower_thread(self):
         t = threading.Thread(target=self.follower)
