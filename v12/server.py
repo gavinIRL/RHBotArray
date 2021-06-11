afbe5d8a89242e477e4d79ded84938da45c11168
Gavin <gavin.feeney01@gmail.com>
2021-06-11 15:03:44 +0100

    Remove scaling from server, change to utils #98

--- v12/server.py
+++ v12/server.py
@@ -79,7 +79,6 @@ class RHBotArrayServer():
         self.move_only = move_only
         self.move_only_exclude_keys = ["a", "s", "d", "f", "g", "h"]
 
-        self.scaling = self.get_monitor_scaling()
         with open("gamename.txt") as f:
             self.gamename = f.readline()
         # initialise the window centre for the mouse resetter
@@ -434,13 +433,6 @@ class RHBotArrayServer():
             # return True for autoloot
             return True
 
-    def get_monitor_scaling(self):
-        user32 = ctypes.windll.user32
-        w_orig = GetSystemMetrics(0)
-        user32.SetProcessDPIAware()
-        [w, h] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
-        return float(("{:.2f}".format(w/w_orig)))
-
     def batch_handle(self, lines: str):
         data = lines.split("\n")
         data.pop(0)
@@ -659,80 +651,6 @@ class RHBotArrayServer():
                     else:
                         CustomInput.release_key(CustomInput.key_map[key], key)
 
-    def filter_blackwhite_invert(self, filter, existing_image):
-        hsv = cv2.cvtColor(existing_image, cv2.COLOR_BGR2HSV)
-        hsv_filter = filter
-        # add/subtract saturation and value
-        h, s, v = cv2.split(hsv)
-        s = self.shift_channel(s, hsv_filter.sAdd)
-        s = self.shift_channel(s, -hsv_filter.sSub)
-        v = self.shift_channel(v, hsv_filter.vAdd)
-        v = self.shift_channel(v, -hsv_filter.vSub)
-        hsv = cv2.merge([h, s, v])
-
-        # Set minimum and maximum HSV values to display
-        lower = np.array([hsv_filter.hMin, hsv_filter.sMin, hsv_filter.vMin])
-        upper = np.array([hsv_filter.hMax, hsv_filter.sMax, hsv_filter.vMax])
-        # Apply the thresholds
-        mask = cv2.inRange(hsv, lower, upper)
-        result = cv2.bitwise_and(hsv, hsv, mask=mask)
-
-        # convert back to BGR
-        img = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
-        # now change it to greyscale
-        grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # now change it to black and white
-        (thresh, blackAndWhiteImage) = cv2.threshold(
-            grayImage, 67, 255, cv2.THRESH_BINARY)
-        # now invert it
-        inverted = (255-blackAndWhiteImage)
-        inverted = cv2.cvtColor(inverted, cv2.COLOR_GRAY2BGR)
-        return inverted
-
-    def shift_channel(self, c, amount):
-        if amount > 0:
-            lim = 255 - amount
-            c[c >= lim] = 255
-            c[c < lim] += amount
-        elif amount < 0:
-            amount = -amount
-            lim = amount
-            c[c <= lim] = 0
-            c[c > lim] -= amount
-        return c
-
-    def detect_level_name(self):
-        wincap = WindowCapture(self.gamename, [1121, 31, 1248, 44])
-        existing_image = wincap.get_screenshot()
-        filter = HsvFilter(0, 0, 0, 169, 34, 255, 0, 0, 0, 0)
-        vision_limestone = Vision('plyr.jpg')
-        # cv2.imwrite("testy2.jpg", existing_image)
-        save_image = vision_limestone.apply_hsv_filter(existing_image, filter)
-        # cv2.imwrite("testy3.jpg", save_image)
-        gray_image = cv2.cvtColor(save_image, cv2.COLOR_BGR2GRAY)
-        (thresh, blackAndWhiteImage) = cv2.threshold(
-            gray_image, 129, 255, cv2.THRESH_BINARY)
-        # now invert it
-        inverted = (255-blackAndWhiteImage)
-        save_image = cv2.cvtColor(inverted, cv2.COLOR_GRAY2BGR)
-        rgb = cv2.cvtColor(save_image, cv2.COLOR_BGR2RGB)
-        tess_config = '--psm 7 --oem 3 -c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-        result = pytesseract.image_to_string(
-            rgb, lang='eng', config=tess_config)[:-2]
-        return result
-
-    def detect_bigmap_open(self):
-        wincap = WindowCapture(self.gamename, custom_rect=[819, 263, 855, 264])
-        image = wincap.get_screenshot()
-        cv2.imwrite("testy.jpg", image)
-        a, b, c = [int(i) for i in image[0][0]]
-        d, e, f = [int(i) for i in image[0][-2]]
-        if a+b+c < 30:
-            if d+e+f > 700:
-                # print("Working")
-                return True
-        return False
-
     def grab_player_pos(self):
         if not self.map_rect:
             wincap = WindowCapture(self.gamename)
@@ -740,7 +658,7 @@ class RHBotArrayServer():
             wincap = WindowCapture(self.gamename, self.map_rect)
         filter = HsvFilter(34, 160, 122, 50, 255, 255, 0, 0, 0, 0)
         image = wincap.get_screenshot()
-        save_image = self.filter_blackwhite_invert(filter, image)
+        save_image = BotUtils.filter_blackwhite_invert(filter, image)
         vision_limestone = Vision('plyr.jpg')
         rectangles = vision_limestone.find(
             save_image, threshold=0.31, epsilon=0.5)
@@ -757,7 +675,7 @@ class RHBotArrayServer():
             return False
 
     def pre_regroup_updates(self):
-        self.level_name = self.detect_level_name()
+        self.level_name = BotUtils.detect_level_name(self.gamename)
         # Then grab the right rect for the level
         try:
             self.map_rect = self.string_to_rect(self.rects[self.level_name])
@@ -773,8 +691,8 @@ class RHBotArrayServer():
                 self.map_rect = [362, 243, 1105, 748]
                 self.speed = 30
         # Then open the map
-        if not self.detect_bigmap_open():
-            self.try_toggle_map()
+        if not BotUtils.detect_bigmap_open():
+            BotUtils.try_toggle_map()
         self.player_pos = self.grab_player_pos()
 
     def regroup(self, coords: str):
