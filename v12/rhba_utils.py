a6285541cac8cd73382cf97cd6df1f4866a9527d
Gavin <gavin.feeney01@gmail.com>
2021-06-12 20:48:42 +0100

    Add find midlevel event to utils

--- v12/rhba_utils.py
+++ v12/rhba_utils.py
@@ -930,6 +930,28 @@ class BotUtils:
                 return points
         return False
 
+    def find_midlevel_event(gamename=False, playerx=False, playery=False):
+        if not gamename:
+            with open("gamename.txt") as f:
+                gamename = f.readline()
+        if not playerx:
+            playerx, playery = BotUtils.grab_player_pos(
+                gamename, [1100, 50, 1260, 210], True)
+        filter = HsvFilter(76, 247, 170, 100, 255, 255, 0, 0, 0, 0)
+        vision = Vision("otherplayerinvert.jpg")
+        wincap = WindowCapture(gamename, [1100, 50, 1260, 210])
+        image = wincap.get_screenshot()
+        image = cv2.blur(image, (4, 4))
+        image = BotUtils.filter_blackwhite_invert(filter, image)
+        rectangles = vision.find(
+            image, threshold=0.61, epsilon=0.5)
+        points = vision.get_click_points(rectangles)
+        if len(points) >= 1:
+            relx = points[0][0] - playerx
+            rely = playery - points[0][1]
+            return relx, rely
+        return False, False
+
     def stop_movement(follower=False):
         if follower:
             follower.pressed_keys = []
