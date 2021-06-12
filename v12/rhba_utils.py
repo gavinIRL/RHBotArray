c90b896eac7fe2a75ed98d5ca1389ba6bb4984e1
Gavin <gavin.feeney01@gmail.com>
2021-06-12 09:31:23 +0100

    Add find enemy to utils #98

--- v12/rhba_utils.py
+++ v12/rhba_utils.py
@@ -904,6 +904,25 @@ class BotUtils:
                 return points
         return False
 
+    def find_enemy(self, gamename, all=False):
+        othr_plyr_vision = Vision("otherplayerinvert.jpg")
+        othr_plyr_wincap = WindowCapture(gamename, [1100, 50, 1260, 210])
+        image = othr_plyr_wincap.get_screenshot()
+        filter = HsvFilter(0, 198, 141, 8, 255, 255, 0, 0, 0, 0)
+        image = cv2.blur(image, (4, 4))
+        image = BotUtils.filter_blackwhite_invert(filter, image)
+        rectangles = othr_plyr_vision.find(
+            image, threshold=0.41, epsilon=0.5)
+        points = othr_plyr_vision.get_click_points(rectangles)
+        if len(points) >= 1:
+            if not all:
+                relx = points[0][0] - 0
+                rely = 0 - points[0][1]
+                return relx, rely
+            else:
+                return points
+        return False
+
 
 class Looting:
     def loot_current_room(gamename, player_name, search_points=False):
