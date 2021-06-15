b9adb70ffc654f7b4f4f1bf64d0e20f7caaa9621
Gavin <gavin.feeney01@gmail.com>
2021-06-15 07:08:19 +0100

    Add basic room format for #101

--- v11/17_room_objs_test.py
+++ v11/17_room_objs_test.py
@@ -1,2 +1,10 @@
 # This file will test how to best store data on each room
 # Will preferably load information from a single file
+
+
+class Room():
+    def __init__(self, file) -> None:
+        # Format of each line in the file should be as follows
+        # actiontype | presleep | postsleep | list-of-coords
+        with open(file) as f:
+            lines = f.readlines()
