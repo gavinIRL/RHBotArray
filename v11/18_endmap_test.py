9cd5da0ed68ffd3d5f7ae0522fb7580f94276abd
Gavin <gavin.feeney01@gmail.com>
2021-06-15 17:01:12 +0100

    Add test prints and sleeps #99

--- v11/18_endmap_test.py
+++ v11/18_endmap_test.py
@@ -10,10 +10,12 @@ def start_endlevel_script():
     # Need to first wait until dungeon check returns false
     while not Events.detect_in_dungeon():
         time.sleep(0.006)
+    print("Got to pre-sectclear detect")
     # Then until sect cleared shows up
     while not BotUtils.detect_sect_clear():
         pydirectinput.press('esc')
-        time.sleep(0.01)
+        time.sleep(0.05)
+    print("Got to pre-endlevel event check")
     # Then wait for end-level event to show up
     start_time = time.time()
     event = False
@@ -31,30 +33,37 @@ def start_endlevel_script():
                 event = True
             break
     # Then do the appropriate handling if event is detected
+    print("Got to pre-event handling")
     if event:
         do_otherworld_handling()
     # Once event is complete move to correct place in room
     move_to_loot_point()
     # And then commence looting
+    print("Got to post-move to loot point")
     while Events.detect_in_dungeon():
         if not loot_everything():
             # Click centre of screen to skip
             skip_to_reward()
             break
+    print("Got to pre-card check")
     # Then wait until card select appears
     while not Events.detect_reward_choice_open():
         time.sleep(0.2)
+    print("Got to pre-choose reward")
     # Then wait until the cards become selectable
     time.sleep(2)
     # Then choose a random card
     Events.choose_random_reward()
     # Then wait until store is detected
+    print("Got to pre-shop check")
     while not Events.detect_store():
         time.sleep(0.2)
+    print("Got to pre-sellrepair")
     # And then perform the sell and repair actions
     sr = SellRepair()
     sr.ident_sell_repair()
     # And then go to next level if needs be
+    print("Got to pre-restart")
     repeat_level()
 
 
