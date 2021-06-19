b45e3fd09218a7267ff6fb2dc640c82bb86ce1cb
Gavin <gavin.feeney01@gmail.com>
2021-06-19 06:41:30 +0100

    Add code to test cooldown checker #99

--- v11/20_check_cooldowns.py
+++ v11/20_check_cooldowns.py
@@ -10,16 +10,16 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 with open("gamename.txt") as f:
     gamename = f.readline()
-wincap = WindowCapture(gamename, [395, 745, 591, 788])
-image = wincap.get_screenshot()
-a, _, _ = [int(i) for i in image[0][0]]
-b, _, _ = [int(i) for i in image[0][39]]
-c, _, _ = [int(i) for i in image[0][78]]
-d, _, _ = [int(i) for i in image[0][117]]
-e, _, _ = [int(i) for i in image[0][156]]
-f, _, _ = [int(i) for i in image[0][195]]
-print("abc:{},{},{} def:{},{},{}".format(a, b, c, d, e, f))
-cv2.imwrite("testytest.jpg", image)
+# wincap = WindowCapture(gamename, [395, 745, 591, 788])
+# image = wincap.get_screenshot()
+# a, _, _ = [int(i) for i in image[0][0]]
+# b, _, _ = [int(i) for i in image[0][39]]
+# c, _, _ = [int(i) for i in image[0][78]]
+# d, _, _ = [int(i) for i in image[0][117]]
+# e, _, _ = [int(i) for i in image[0][156]]
+# f, _, _ = [int(i) for i in image[0][195]]
+# print("abc:{},{},{} def:{},{},{}".format(a, b, c, d, e, f))
+# cv2.imwrite("testytest.jpg", image)
 
 
 def check_off_cooldown(gamename):
@@ -45,3 +45,9 @@ def check_off_cooldown(gamename):
     if f != 142:
         cd_list[5] = True
     return cd_list
+
+
+start = time.time()
+print(check_off_cooldown(gamename))
+total = time.time()-start
+print("Time taken: {}s".format(total))
