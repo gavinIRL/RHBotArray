98266030c6dfd0376cc6697850beb77b232b1d10
Gavin <gavin.feeney01@gmail.com>
2021-06-19 06:38:59 +0100

    Add proper function for cd check #99

--- v11/20_check_cooldowns.py
+++ v11/20_check_cooldowns.py
@@ -20,3 +20,28 @@ e, _, _ = [int(i) for i in image[0][156]]
 f, _, _ = [int(i) for i in image[0][195]]
 print("abc:{},{},{} def:{},{},{}".format(a, b, c, d, e, f))
 cv2.imwrite("testytest.jpg", image)
+
+
+def check_off_cooldown(gamename):
+    cd_list = [False, False, False, False, False, False]
+    wincap = WindowCapture(gamename, [395, 745, 591, 788])
+    image = wincap.get_screenshot()
+    a, _, _ = [int(i) for i in image[0][0]]
+    b, _, _ = [int(i) for i in image[0][39]]
+    c, _, _ = [int(i) for i in image[0][78]]
+    d, _, _ = [int(i) for i in image[0][117]]
+    e, _, _ = [int(i) for i in image[0][156]]
+    f, _, _ = [int(i) for i in image[0][195]]
+    if a != 56:
+        cd_list[0] = True
+    if b != 11:
+        cd_list[1] = True
+    if c != 44:
+        cd_list[2] = True
+    if d != 245:
+        cd_list[3] = True
+    if e != 231:
+        cd_list[4] = True
+    if f != 142:
+        cd_list[5] = True
+    return cd_list
