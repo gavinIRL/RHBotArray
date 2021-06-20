41fdd8f7da16c8a1d78e62110e2eca5f6e5cd76f
Gavin <gavin.feeney01@gmail.com>
2021-06-20 11:23:24 +0100

    Add even more accurate loot detect config #99

--- v11/22_rgb_2line_filt_test.py
+++ v11/22_rgb_2line_filt_test.py
@@ -7,7 +7,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open("gamename.txt") as f:
     gamename = f.readline()
 
-vision = Vision("lootline.jpg")
+vision = Vision("doublelootline.jpg")
 wincap = WindowCapture(gamename)
 
 start = time.time()
