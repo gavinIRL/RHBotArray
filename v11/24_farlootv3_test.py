f51e1c81bf99924007b68c1dd1e3faf2a22658f9
Gavin <gavin.feeney01@gmail.com>
2021-06-21 16:30:01 +0100

    Add LHS loot tag detect #99

--- v11/24_farlootv3_test.py
+++ v11/24_farlootv3_test.py
@@ -26,3 +26,25 @@ def grab_lootposv3_right():
         center_y = y + int(h/2)
         points.append((center_x, center_y))
     return points
+
+
+def grab_lootposv3_right():
+    with open("gamename.txt") as f:
+        gamename = f.readline()
+    vision = Vision("lootside.jpg")
+    rect = [5, 80, 1273, 776]
+    wincap = WindowCapture(gamename, rect)
+    screenshot = wincap.get_screenshot()
+    original_image = screenshot
+    rectangles = vision.find(
+        original_image, threshold=0.81, epsilon=0.5)
+    if len(rectangles) < 1:
+        return False
+    points = []
+    for (x, y, w, h) in rectangles:
+        x += rect[0]
+        y += rect[1]
+        center_x = x + int(w/2)
+        center_y = y + int(h/2)
+        points.append((center_x, center_y))
+    return points
