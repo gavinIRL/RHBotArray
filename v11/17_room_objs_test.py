ae9117a8bd74fe62e07cc15981d4aa3eca33c2c6
Gavin <gavin.feeney01@gmail.com>
2021-06-26 20:49:36 +0100

    Add skeleton of room object logic #101

--- v11/17_room_objs_test.py
+++ v11/17_room_objs_test.py
@@ -1,10 +1,35 @@
 # This file will test how to best store data on each room
 # Will preferably load information from a single file
+import os
 
 
 class Room():
-    def __init__(self, file) -> None:
+    def __init__(self, line) -> None:
         # Format of each line in the file should be as follows
-        # actiontype | presleep | postsleep | list-of-coords
-        with open(file) as f:
-            lines = f.readlines()
+        # coords_n | coords_n+1 # action_n | action_n+1 # tags
+        # format coords is x,y
+        # ------------------------------------
+        # list actions is as follows:
+        # clearl - position to perform clear from, point left (r,u,d)
+        # exit - position to exit room from
+        # chestl - position to attack chest, point left (r,u,d)
+        # reposl - position to reposition to, point left (r,u,d)
+        # ------------------------------------
+        # list of tags is as follows
+        # nxtbssl - next room is the boss room, hold l to enter (r,u,d)
+        # curbss - this room is the boss room
+        pass
+
+
+def load_rooms(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    list_rooms = []
+    for line in lines:
+        list_rooms.append(Room(line))
+    return list_rooms
+
+
+filename = os.path.dirname(
+    os.path.abspath(__file__)) + "/levels/map10updated.txt"
+rooms = load_rooms(filename)
