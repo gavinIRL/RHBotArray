246a983b6b8e1bf208da458df5af24cb1a32a950
Gavin <gavin.feeney01@gmail.com>
2021-06-27 08:17:02 +0100

    Add further tags and actions to room obj #101

--- v11/17_room_objs_test.py
+++ v11/17_room_objs_test.py
@@ -4,20 +4,25 @@ import os
 
 
 class Room():
-    def __init__(self, line) -> None:
+    def __init__(self, line: str) -> None:
         # Format of each line in the file should be as follows
         # coords_n | coords_n+1 # action_n | action_n+1 # tags
         # format coords is x,y
         # ------------------------------------
         # list actions is as follows:
         # clearl - position to perform clear from, point left (r,u,d)
+        # bossl - position to perform boss attacks from (r,u,d)
         # exit - position to exit room from
         # chestl - position to attack chest, point left (r,u,d)
         # reposl - position to reposition to, point left (r,u,d)
+        # loot - position to attempt to loot from
+        # wypt - this is a travel waypoint only
         # ------------------------------------
         # list of tags is as follows
         # nxtbssl - next room is the boss room, hold l to enter (r,u,d)
         # curbss - this room is the boss room
+        # nocmbt - this room is just for walking through
+        coords, actions, tags = line.split("#")
         pass
 
 
