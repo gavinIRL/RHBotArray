743244eef9bbfbb19c23dd039d391ff65f20f68c
Gavin <gavin.feeney01@gmail.com>
2021-06-30 22:28:46 +0100

    Add anti-stick upgrade to test25 #110

--- v11/25_map10_script_v2.py
+++ v11/25_map10_script_v2.py
@@ -71,7 +71,8 @@ class Map10_MS30():
         time.sleep(0.25)
         room = self.rooms[0]
         # BotUtils.move_diagonal(int(room[1]), int(room[2]), self.speed)
-        outcome = BotUtils.move_bigmap_dynamic(int(room[1]), int(room[2]))
+        outcome = AntiStickUtils.move_bigmap_dynamic(
+            int(room[1]), int(room[2]))
         nodetcnt = 0
         while not outcome:
             nodetcnt += 1
@@ -81,7 +82,8 @@ class Map10_MS30():
             key = "right"
             CustomInput.press_key(CustomInput.key_map[key], key)
             CustomInput.release_key(CustomInput.key_map[key], key)
-            outcome = BotUtils.move_bigmap_dynamic(int(room[1]), int(room[2]))
+            outcome = AntiStickUtils.move_bigmap_dynamic(
+                int(room[1]), int(room[2]))
         time.sleep(0.4)
         self.roomclear_skill()
         time.sleep(0.6)
@@ -116,7 +118,8 @@ class Map10_MS30():
         # print("{},{}".format(int(room[1]), int(room[2])))
         # print("Just about to move to next roomstart, room {}".format(num))
         # BotUtils.move_diagonal(int(room[3]), int(room[4]), self.speed)
-        outcome = BotUtils.move_bigmap_dynamic(int(room[3]), int(room[4]))
+        outcome = AntiStickUtils.move_bigmap_dynamic(
+            int(room[3]), int(room[4]))
         nodetcnt = 0
         while not outcome:
             nodetcnt += 1
@@ -127,7 +130,8 @@ class Map10_MS30():
                 key = "right"
                 CustomInput.press_key(CustomInput.key_map[key], key)
                 CustomInput.release_key(CustomInput.key_map[key], key)
-            outcome = BotUtils.move_bigmap_dynamic(int(room[3]), int(room[4]))
+            outcome = AntiStickUtils.move_bigmap_dynamic(
+                int(room[3]), int(room[4]))
         # print("Finished moving to next roomstart, room {}".format(num))
         time.sleep(0.4)
         # Now calculate the travel time to figure out
@@ -142,7 +146,8 @@ class Map10_MS30():
         room = self.rooms[num]
         # print("Trvel time for room {} is {}s".format(num, travel_time))
         # BotUtils.move_diagonal(int(room[1]), int(room[2]), self.speed)
-        outcome = BotUtils.move_bigmap_dynamic(int(room[1]), int(room[2]))
+        outcome = AntiStickUtils.move_bigmap_dynamic(
+            int(room[1]), int(room[2]))
         nodetcnt = 0
         while not outcome:
             nodetcnt += 1
@@ -152,7 +157,8 @@ class Map10_MS30():
             key = "right"
             CustomInput.press_key(CustomInput.key_map[key], key)
             CustomInput.release_key(CustomInput.key_map[key], key)
-            outcome = BotUtils.move_bigmap_dynamic(int(room[1]), int(room[2]))
+            outcome = AntiStickUtils.move_bigmap_dynamic(
+                int(room[1]), int(room[2]))
         if travel_time < 1.8:
             time.sleep(0.6)
         self.roomclear_skill()
@@ -417,121 +423,6 @@ class Map10_MS30():
                 CustomInput.key_map[second_key], second_key)
         return False
 
-    # Everything below this is for endmap handling from test 18
-    # def start_endlevel_script(self, gamename, repeat=False):
-    #     # Need to first wait until dungeon check returns false
-    #     while Events.detect_in_dungeon():
-    #         time.sleep(0.006)
-    #     # print("Got to pre-sectclear detect")
-    #     # Then until sect cleared shows up
-    #     while not BotUtils.detect_sect_clear():
-    #         if Events.detect_move_reward_screen(gamename):
-    #             break
-    #         if Events.detect_endlevel_bonus_area(gamename):
-    #             break
-    #         pydirectinput.press('esc')
-    #         time.sleep(0.05)
-    #         BotUtils.close_map_and_menu(gamename)
-    #         time.sleep(0.25)
-    #         # print("Stuck in the first loop")
-    #     # print("Got to pre-endlevel event check")
-    #     # Then wait for end-level event to show up
-    #     start_time = time.time()
-    #     event = False
-    #     while True:
-    #         time.sleep(0.006)
-    #         if time.time() > start_time + 2.5:
-    #             print("Exited loop #2 due to timer")
-    #             break
-    #         if Events.detect_move_reward_screen(gamename):
-    #             break
-    #         if Events.detect_endlevel_bonus_area(gamename):
-    #             event = True
-    #             break
-    #         if not Events.detect_in_dungeon():
-    #             # Press escape
-    #             pydirectinput.press('esc')
-    #             time.sleep(0.05)
-    #             BotUtils.close_map_and_menu(gamename)
-    #             # Wait 2 seconds
-    #             time.sleep(0.5)
-    #             # Then if ok is detected turn flag on
-    #             if Events.detect_endlevel_bonus_area(gamename):
-    #                 event = True
-    #             break
-    #     # print("Got to pre-endlevel event check")
-    #     # Then wait for end-level event to show up
-    #     event = False
-    #     start_time = time.time()
-    #     while True:
-    #         time.sleep(0.006)
-    #         if time.time() - start_time > 3.5:
-    #             break
-    #         if not Events.detect_in_dungeon():
-    #             # Press escape
-    #             pydirectinput.press('esc')
-    #             # Wait 2 seconds
-    #             time.sleep(0.5)
-    #             if Events.detect_move_reward_screen(gamename):
-    #                 # print("Managed to detect the skip to reward screen option #2")
-    #                 break
-    #             # Then if ok is detected turn flag on
-    #             if Events.detect_endlevel_bonus_area(gamename):
-    #                 event = True
-    #             break
-    #     # Then do the appropriate handling if event is detected
-    #     # print("Got to pre-event handling")
-    #     if event:
-    #         self.do_otherworld_handling(gamename)
-    #         time.sleep(1)
-    #     Looting.grab_nearby_loot(gamename)
-    #     # Once event is complete move to correct place in room
-    #     self.move_to_loot_point()
-    #     # And then commence looting
-    #     print("Got to post-move to loot point")
-    #     while Events.detect_in_dungeon():
-    #         if not self.loot_everything(gamename):
-    #             # Try once more to loot
-    #             Looting.grab_nearby_loot(gamename)
-    #             self.loot_everything(gamename)
-    #             # And one final time
-    #             pydirectinput.press("right")
-    #             Looting.grab_nearby_loot(gamename)
-    #             self.loot_everything(gamename)
-    #             # Click centre of screen to skip
-    #             self.skip_to_reward(gamename)
-    #             # Need to double click this to make sure
-    #             self.skip_to_reward(gamename)
-    #             break
-    #     # print("Got to pre-card check")
-    #     # Then wait until card select appears
-    #     while not Events.detect_reward_choice_open(gamename):
-    #         time.sleep(0.2)
-    #     # print("Got to pre-choose reward")
-    #     # Then wait until the cards become selectable
-    #     time.sleep(4)
-    #     # Then choose a random card
-    #     Events.choose_random_reward(gamename)
-    #     # Then wait until store is detected
-    #     # print("Got to pre-shop check")
-    #     while not Events.detect_store(gamename):
-    #         time.sleep(0.2)
-    #     # print("Got to pre-sellrepair")
-    #     # Then wait to see if chest event appears
-    #     time.sleep(2)
-    #     if Events.detect_endlevel_chest(gamename):
-    #         pydirectinput.press('esc')
-    #         time.sleep(0.05)
-    #     # Then check for loot one last time
-    #     self.check_loot_preshop(gamename)
-    #     # And then perform the sell and repair actions
-    #     sr = SellRepair()
-    #     sr.ident_sell_repair()
-    #     # And then go to next level if needs be
-    #     # print("Got to pre-restart")
-    #     if repeat:
-    #         self.repeat_level(gamename)
-
     def do_otherworld_handling(self, gamename):
         time.sleep(0.4)
         RHClick.click_otherworld_ok(gamename)
@@ -593,22 +484,22 @@ class Map10_MS30():
 
     def navigate_otherworld_loot(self, gamename):
         # BotUtils.try_toggle_map_clicking()
-        result = BotUtils.move_bigmap_dynamic(593, 419)
+        result = AntiStickUtils.move_bigmap_dynamic(593, 419)
         while not result:
             BotUtils.try_toggle_map_clicking()
-            result = BotUtils.move_bigmap_dynamic(593, 419)
-        BotUtils.move_bigmap_dynamic(667, 392)
+            result = AntiStickUtils.move_bigmap_dynamic(593, 419)
+        AntiStickUtils.move_bigmap_dynamic(667, 392)
         while not result:
             BotUtils.try_toggle_map_clicking()
-            result = BotUtils.move_bigmap_dynamic(667, 392)
+            result = AntiStickUtils.move_bigmap_dynamic(667, 392)
         BotUtils.close_map_and_menu(gamename)
         self.loot_everything(gamename)
         # Then have a second bite at looting
-        BotUtils.move_bigmap_dynamic(667, 407)
+        AntiStickUtils.move_bigmap_dynamic(667, 407)
         self.loot_everything(gamename)
 
     def leave_otherworld(self, gamename):
-        BotUtils.move_bigmap_dynamic(667, 455)
+        AntiStickUtils.move_bigmap_dynamic(667, 455)
         time.sleep(0.5)
         RHClick.click_yes(gamename)
         time.sleep(2.5)
@@ -636,7 +527,7 @@ class Map10_MS30():
         return False
 
     def move_to_loot_point(self):
-        BotUtils.move_bigmap_dynamic(1038, 617, checkmap=False)
+        AntiStickUtils.move_bigmap_dynamic(1038, 617, checkmap=False)
 
     def check_loot_preshop(self, gamename):
         # Move to primary lootpoint
