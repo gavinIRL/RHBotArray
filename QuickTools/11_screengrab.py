a0ab40f355ba5882a10181c981d7600b6dcb5486
Gavin <gavin.feeney01@gmail.com>
2021-07-05 20:19:10 +0100

    Add util for grabbing simple screenshots

---
+++ QuickTools/11_screengrab.py
@@ -0,0 +1,11 @@
+from windowcapture import WindowCapture
+import os
+import cv2
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with open("gamename.txt") as f:
+    gamename = f.readline()
+# rect=[415, 533, 888, 700])
+wincap = WindowCapture(gamename)
+image = wincap.get_screenshot()
+cv2.imwrite("testytest.jpg", image)
