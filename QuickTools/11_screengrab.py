d3efa2acfe04678e1bfe016928838c999a2d3284
Gavin <gavin.feeney01@gmail.com>
2021-07-05 20:38:39 +0100

    Remove border from screengrab

--- QuickTools/11_screengrab.py
+++ QuickTools/11_screengrab.py
@@ -7,5 +7,6 @@ with open("gamename.txt") as f:
     gamename = f.readline()
 # rect=[415, 533, 888, 700])
 wincap = WindowCapture(gamename)
+wincap.update_window_position(False)
 image = wincap.get_screenshot()
 cv2.imwrite("testytest.jpg", image)
